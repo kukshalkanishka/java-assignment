c6593c13f5f67a88aec11b55629c902fd8683e66
KKanishk <kkanishk@in-KKanishk.local>
2019-03-07 12:29:21 +0530

    Add solution for calculating cuboid volume

--- src/com/step/assignments/Cuboid.java
+++ src/com/step/assignments/Cuboid.java
@@ -1,4 +1,21 @@
 package com.step.assignments;
 
-public class Cuboid {
+public class Cuboid extends Rectangle {
+     private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+        if(this.height < 0){
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return super.getArea() * this.height;
+    }
 }
--- src/com/step/assignments/Rectangle.java
+++ src/com/step/assignments/Rectangle.java
@@ -1,4 +1,30 @@
 package com.step.assignments;
 
 public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+        if(width<0){
+            this.width = 0;
+        }
+        if(length<0){
+            this.length = 0;
+        }
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getArea(){
+        return this.width * this.length;
+    }
 }
