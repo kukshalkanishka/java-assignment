ac39da9d54e474c9781524d560d5a0c4cab58f17
KKanishk <kkanishk@in-KKanishk.local>
2019-03-11 11:10:12 +0530

    Add tests for iterator

---
+++ test/com/step/linkedList/IteratorTest.java
@@ -0,0 +1,16 @@
+package com.step.linkedList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IteratorTest {
+
+    @Test
+    void hasNext() {
+    }
+
+    @Test
+    void next() {
+    }
+}
